c6af689ca681dd559ea9c5112cb945dd0a7111a5
Youssif Abdelrazek <youssifh488@gmail.com>
2025-08-07 07:50:52 +0300

    Temp rename to fix folder case issue

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ScreenText from "../components/ScreenText";
+import ScreenText from "@/components/ScreenText";
 import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
