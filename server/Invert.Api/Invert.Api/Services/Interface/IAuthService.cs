eb3987d86b017930a77338492709e3efb82e5ec9
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-21 15:09:17 +0300

    Implement user management in AuthService and IAuthService Added AuthService class to manage user operations such as retrieving users, checking existence, creating users, and validating passwords using ASP.NET Core Identity. Defined IAuthService interface to outline user management methods.

---
+++ server/Invert.Api/Invert.Api/Services/Interface/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using Invert.Api.Dtos;
+using Invert.Api.Entities;
+
+namespace Invert.Api.Services.Interface
+{
+    public interface IAuthService
+    {
+        Task<AppUser> GetUserByNameAsync(string userName);
+        //get user by email
+        Task<AppUser> GetUserByEmailAsync(string email);
+        Task<bool> UserExistsAsync(string userName);
+        Task<bool> EmailExistsAsync(string email);
+        Task<bool> CheckPasswordAsync(AppUser user, string password);
+        Task CreateUserAsync(AppUser user, string password);
+        Task UpdateUserAsync(AppUser user);
+        Task<string> GenerateJwtTokenAsync(AppUser user);
+        Task<string> GenerateRefreshTokenAsync();
+    }
+}
