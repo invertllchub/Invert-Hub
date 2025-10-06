a6656b7e623445d0fb3c7845d2412e1b17fb0767
Mohamed Tharwat <mthawrat@example.com>
2025-10-06 15:12:02 +0300

    fix upload to cloudinary fn

--- client/utils/CloudinaryUpload.ts
+++ client/utils/CloudinaryUpload.ts
@@ -1,10 +1,11 @@
-// utils/CloudinaryUpload.ts
-export async function uploadToCloudinary(file: File): Promise<string> {
+// utils/cloudinaryUpload.ts
+
+export async function uploadToCloudinary(
+  file: File,
+  resourceType: "image" | "video" |"raw" = "image"
+): Promise<string> {
   const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!;
   const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!;
-
-  // تحديد نوع الملف تلقائيًا
-  const resourceType = file.type.startsWith("image/") ? "image" : "raw";
 
   const formData = new FormData();
   formData.append("file", file);
@@ -18,10 +19,7 @@ export async function uploadToCloudinary(file: File): Promise<string> {
     }
   );
 
-  if (!res.ok) {
-    const errorData = await res.json().catch(() => ({}));
-    throw new Error(errorData.error?.message || "Upload failed");
-  }
+  if (!res.ok) throw new Error("Upload failed");
 
   const data = await res.json();
   return data.secure_url; 
