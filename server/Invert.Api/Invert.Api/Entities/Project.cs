0d61ee0829f277769bc289a36fa07653ca4186cc
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-10-18 12:12:53 +0300

    Update Articles Entity

--- server/Invert.Api/Invert.Api/Entities/Project.cs
+++ server/Invert.Api/Invert.Api/Entities/Project.cs
@@ -7,16 +7,11 @@ namespace Invert.Api.Entities
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
-        public string? AnimatedText { get; set; } = string.Empty;
-        public string? Overview { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        //public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string? pathImg { get; set; } = string.Empty;
 
         public bool IsActive { get; set; }
 
-        //array of images
-        public List<string>? Images { get; set; }
-        public ICollection<Fact> Facts { get; set; } = new List<Fact>();
 
     }
 }
