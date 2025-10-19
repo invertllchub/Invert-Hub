efa36a9146d05008999560af81a86ea36fab2061
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-10-20 00:34:12 +0300

    Update Auto Mapper Profile

--- server/Invert.Api/Invert.Api/Mappings/AutoMapperProfile.cs
+++ server/Invert.Api/Invert.Api/Mappings/AutoMapperProfile.cs
@@ -1,8 +1,9 @@
-﻿using Invert.Api.Dtos;
-using Invert.Api.Entities;
+﻿using Invert.Api.Entities;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using AutoMapper;
 using Invert.Api.Dtos.Article;
+using Invert.Api.Dtos.Project;
+using Invert.Api.Dtos.Job;
 
 namespace Invert.Api.Mappings
 {
@@ -13,6 +14,16 @@ namespace Invert.Api.Mappings
             CreateMap<Project, ProjectDto>();
             //.ForMember(d => d.Images, o => o.MapFrom(s => s.Images.Select(i => i.Url)));
             CreateMap<CreateProjectDto, Project>();
+
+            CreateMap<CreateJobDto, Job>()
+             .ForMember(dest => dest.Requirements, opt => opt.Ignore());// Handle manually
+                                                                        // Similar for others
+            CreateMap<Job, JobDto>()
+                .ForMember(dest => dest.Requirements, opt => opt.MapFrom(src => src.Requirements.Split(';', StringSplitOptions.RemoveEmptyEntries)));
+            // Similar for Skills, Benefits
+
+            CreateMap<CreateArticleDto, Article>();
+            CreateMap<Article, ArticleDto>();
         }
     }
 }
