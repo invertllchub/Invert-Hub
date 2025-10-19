5fffc4d0c2a42ec3d1c71d494056928957fcc606
Mohamed Tharwat <mthawrat@example.com>
2025-10-19 17:04:14 +0300

    some more responsive updates

--- client/app/(dashboard)/dashboard/projects/add/page.tsx
+++ client/app/(dashboard)/dashboard/projects/add/page.tsx
@@ -6,21 +6,21 @@ import AddProjectForm from "@/components/dashboard/projectsComponents/AddProject
 export default function AddProjectPage() {
 
   return (
-    <div className="pl-30 pr-15 py-10 bg-gray-200/75 overflow-hidden">
-        <div className="w-full flex flex-col md:flex-row items-center justify-between gap-6">
+    <div className="pl-0 md:pl-30 pr-0 md:pr-15 py-10 bg-gray-200/75 overflow-hidden">
+        <div className="w-full flex md:flex-row items-center justify-between gap-6 p-6 md:p-12">
           <h1 className="text-2xl md:text-4xl font-extrabold text-gray-800">
             ADD YOUR PROJECT
           </h1>
           <button
             form="add-project-form"
             type="submit"
-            className="px-6 w-20 py-2 bg-blue-600 hover:bg-blue-500 text-white rounded-lg col-start-1 cursor-pointer"
+            className="px-4 w-42 py-2 bg-blue-600 hover:bg-blue-700 transition-colors text-white rounded-lg col-start-1 cursor-pointer"
           >
-            Save
+            Save Changes
           </button>
         </div>
 
-        <div className="w-full flex items-start justify-between gap-4">
+        <div className="mb-10">
           <AddProjectForm />
         </div>
     </div>
