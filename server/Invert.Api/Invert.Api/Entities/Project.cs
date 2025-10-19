aaa5bbebbe7072111d389400e206a9e9f66547b4
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-10-20 00:31:19 +0300

    Add Job Model and Update Project

--- server/Invert.Api/Invert.Api/Entities/Project.cs
+++ server/Invert.Api/Invert.Api/Entities/Project.cs
@@ -7,11 +7,10 @@ namespace Invert.Api.Entities
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
-        //public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public string? pathImg { get; set; } = string.Empty;
-
-        public bool IsActive { get; set; }
-
+        public string? PathImg { get; set; } = string.Empty;
+        public string Link { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
 
     }
 }
