b0ccb4ea987324b667ea0588bd96ec4eb7664740
Mohamed Tharwat <mthawrat@example.com>
2025-10-20 16:55:41 +0300

    update login

--- client/components/forms/LoginForm.tsx
+++ client/components/forms/LoginForm.tsx
@@ -4,58 +4,72 @@ import Link from 'next/link'
 // React-hook-form and validation with Zod
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { LoginSchema } from '../schemas/LoginSchema';
 import { FormFields } from '../schemas/LoginSchema';
+import { LoginSchema } from '../schemas/LoginSchema';
 // Toast
 import { showToast } from "@/components/jobs/Toast";
+// Next Navigation
+import { useRouter } from "next/navigation";
 
 function LoginForm() {
+    const router = useRouter()
     const {
         register,
-        handleSubmit,
         reset,
-        setValue,
-        formState: { errors },
+        handleSubmit,
+        formState: { errors, isSubmitting }
     } = useForm<FormFields>({
-        resolver: zodResolver(LoginSchema),
-    });
+        resolver: zodResolver(LoginSchema)
+    })
 
     const onSubmit: SubmitHandler<FormFields> = async (data) => {
-    
         const toastId = showToast("loading", {
-            message: "Please wait seconds to login..",
+            message: "Please wait a few seconds to login...",
         });
 
         try {
-            const formData = new FormData();
-            formData.append("userName", data.userName);
-            formData.append("passwoed", data.password);
-
-            const response = await fetch("", {
+            const response = await fetch("/api/login", {
                 method: "POST",
-                body: formData,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
             });
 
             const result = await response.json();
 
             if (result.success) {
-                reset();
-            } 
+                showToast("error", {
+                    message: result.message || "Login failed!",
+                    toastId,
+                });
+                return;
+            }
 
+            const { token } = result;
+
+            document.cookie = `token=${token}; path=/; max-age=86400;`;
+
+            showToast("success", {
+                message: "Login has been done successfully!",
+                toastId,
+            });
+
+            reset();
+
+            router.push("/dashboard");
         } catch (error) {
             console.error(error);
             showToast("error", {
-                message: "Failed to login",
+                message: "Failed to login, please try again later.",
                 toastId,
             });
         }
     };
 
     return (
-        <form
-        onSubmit={handleSubmit(onSubmit)}
-        >
-            <div className="flex flex-col gap-4 w-80 mx-auto p-4">
+        <form onSubmit={handleSubmit(onSubmit)} >
+            <div className="flex flex-col gap-4 w-85 mx-auto p-4">
                 <div className='w-full'>
                     <label htmlFor="userName">User Name</label>
                     <input 
@@ -82,22 +96,24 @@ function LoginForm() {
                         <div className='text-red-600'>{errors.password.message}</div>
                     )}
                 </div>
-            </div>
-            <button 
-            type='submit'
-            className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md 
-            hover:bg-blue-700 hover:shadow-lg cursor-pointer mt-4
-            focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1
-            transition duration-200"
-            >
-                Login
-            </button>
-            <p className="text-sm text-gray-600">
+                <button 
+                type='submit'
+                className={`w-full text-white font-semibold py-2 px-4 rounded-lg shadow-md 
+                mt-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1
+                transition duration-200 ${isSubmitting
+                    ? "bg-blue-400 cursor-not-allowed"
+                    : "bg-blue-600 hover:bg-blue-700 hover:shadow-lg cursor-pointer"
+                }`}
+                >
+                    {isSubmitting ? 'Logining' : 'Login'}
+                </button>
+                <p className="text-sm text-gray-600">
                 Not registered yet?{" "}
-                <Link href="/Signup" className="text-blue-600 font-medium hover:underline">
-                    Create an account  
-                </Link>
-            </p>
+                    <Link href="/Signup" className="text-blue-600 font-medium hover:underline">
+                        Create an account  
+                    </Link>
+                </p>
+            </div>
         </form>
     )
 }
--- client/components/schemas/LoginSchema.tsx
+++ client/components/schemas/LoginSchema.tsx
@@ -1,8 +1,14 @@
-import { z } from "zod";
+
+import {z} from 'zod';
 
 export const LoginSchema = z.object({
-    userName: z.string().min(1, "Username is required"),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
-});
+    userName: z
+    .string("Username is required")
+    .min(5, "Username must be at least 5 characters long"),
+    password: z
+    .string("Password is required")
+    .min(8, "Password must be at least 8 characters long")
+})
 
-export type FormFields = z.infer<typeof LoginSchema>;
+export type FormFields = z.infer<typeof LoginSchema>
+
